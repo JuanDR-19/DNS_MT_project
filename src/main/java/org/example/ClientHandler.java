ae29154a92a9b256742a05211bca92c6376b8fe8
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-10-22 18:26:37 -0500

    configuracion de headers (constructores) y se mejoro el procesar mensaje

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -9,14 +10,14 @@ public class ClientHandler extends Thread implements Runnable {
     private Socket cliente;
     private DatagramPacket input;
     private DatagramSocket SocketUDP;
-    private static final int UDP_SIZE=1024;
+    private static final int UDP_SIZE = 1024;
 
-    private static final int PUERTO_ENVIO=53;
+    private static final int PUERTO_ENVIO = 53;
 
-    private static ArrayList<String> Addresses= new ArrayList<>();
-    private static ArrayList<String> Names= new ArrayList<>();
-    private static ArrayList<String> Tipos= new ArrayList<>();
-    private static ArrayList<String> Groups= new ArrayList<>();
+    private static ArrayList<String> Addresses = new ArrayList<>();
+    private static ArrayList<String> Names = new ArrayList<>();
+    private static ArrayList<String> Tipos = new ArrayList<>();
+    private static ArrayList<String> Groups = new ArrayList<>();
     private static final InetAddress IPJAVERIANADNS;
 
     static {
@@ -28,24 +29,26 @@ public class ClientHandler extends Thread implements Runnable {
         }
     }
 
-    public ClientHandler(Socket socket, ArrayList<String> Addresses1, ArrayList<String> Names1, ArrayList<String> Tipos1, ArrayList<String> Groups1) throws SocketException {
-        cliente=socket;
-        try{
-            SocketUDP= new DatagramSocket(PUERTO_ENVIO);
-        }catch(Exception e){
+    public ClientHandler(Socket socket, ArrayList<String> Addresses1, ArrayList<String> Names1,
+                         ArrayList<String> Tipos1, ArrayList<String> Groups1) throws SocketException {
+        cliente = socket;
+        try {
+            SocketUDP = new DatagramSocket(PUERTO_ENVIO);
+        } catch (Exception e) {
             System.out.println("Fallo al abrir el socket, revisar el stack trace: ");
             e.printStackTrace();
         }
-        Addresses=Addresses1;
-        Names=Names1;
-        Tipos=Tipos1;
-        Groups= Groups1;
+        Addresses = Addresses1;
+        Names = Names1;
+        Tipos = Tipos1;
+        Groups = Groups1;
     }
 
-    public void run(){
-        String mensaje="";
-        System.out.println("INICIO DE LECTURA POR EL SOCKET EN PUERTO "+SocketUDP.getLocalPort()+" "+cliente.getLocalPort());
-        do{
+    public void run() {
+        String mensaje = "";
+        System.out.println(
+                "INICIO DE LECTURA POR EL SOCKET EN PUERTO " + SocketUDP.getLocalPort() + " " + cliente.getLocalPort());
+        do {
 
             byte[] in = new byte[UDP_SIZE];
             DatagramPacket indp = new DatagramPacket(in, UDP_SIZE);
@@ -53,81 +56,124 @@ public class ClientHandler extends Thread implements Runnable {
             try {
                 SocketUDP.receive(indp);
                 System.out.println("Se recibió un datagrama");
-                mensaje =new String(indp.getData(),0,indp.getLength());
-                //aca se mostraria el mensaje que llega por datagrama
+                mensaje = new String(indp.getData(), 0, indp.getLength());
+                // aca se mostraria el mensaje que llega por datagrama
                 this.procesarMensaje(indp);
             } catch (IOException e) {
                 System.out.println("Error! al leer el datagrama enviado: ");
                 throw new RuntimeException(e);
             }
-        }while(!mensaje.equals("Quit"));
-        if(mensaje.equals("Quit")){
-            System.err.println("No se encontro la direccion para el dominio solicitado (ni en el servidor instaurado ni en el servidor de la javeriana)");
+        } while (!mensaje.equals("Quit"));
+        if (mensaje.equals("Quit")) {
+            System.err.println(
+                    "No se encontro la direccion para el dominio solicitado (ni en el servidor instaurado ni en el servidor de la javeriana)");
         }
     }
 
     public void procesarMensaje(DatagramPacket indp) throws IOException {
-        //TODO
-        String mensaje =new String(indp.getData(),0,indp.getLength());
-        byte [] queries= mensaje.getBytes();
-        DataInputStream query = new DataInputStream(new ByteArrayInputStream(queries));
 
-        Header queryRecibida = new Header();
-        short ID= query.readShort(); // se usa short por lo que id es un espacio de 16 bits que se necesitan
-        short flags = query.readByte();
-        //int Qr = ;
-        //int opCode =;
-        //int aa=;
-        //int tc=;
-        //int rd=;
-
+        // https://levelup.gitconnected.com/dns-response-in-java-a6298e3cc7d9
+            //codigo adaptado del enlace anterior
+            //adaptacion de la implementación del datainputstream y sus funciones
 
         /*
-            *PASOS A SEGUIR
-                *recuperar de ese string el nombre que se deea buscar
-                * recuperar los datos que se quieren obtener (como el header y los demás flags del paquete)
-                * comparar el string de nombre encontrado con los registrados en la memoria
-                * String ip= ...
-                * String nombre= ...
-                * String ipencontrada = GetIP(nombre);
-                * crear un tipo Mensaje con los datos recuperados del id
-                * enviar por el socket cliente el paquete creado
-                    *DatagramPacket datagramaEnviar = new DatagramPacket(respuesta.getBytes(), respuesta.length(), datagramaRecibido.getAddress(), datagramaRecibido.getPort());
-                    * SocketUDP.send(datagramaEnviar);
+         * PASOS A SEGUIR
+         * recuperar de ese string el nombre que se deea buscar
+         * recuperar los datos que se quieren obtener (como el header y los demás flags
+         * del paquete)
+         * comparar el string de nombre encontrado con los registrados en la memoria
+         * String ip= ...
+         * String nombre= ...
+         * String ipencontrada = GetIP(nombre);
+         * crear un tipo Mensaje con los datos recuperados del id
+         * enviar por el socket cliente el paquete creado
+         * DatagramPacket datagramaEnviar = new DatagramPacket(respuesta.getBytes(),
+         * respuesta.length(), datagramaRecibido.getAddress(),
+         * datagramaRecibido.getPort());
+         * SocketUDP.send(datagramaEnviar);
          */
+
+        String mensaje = new String(indp.getData(), 0, indp.getLength());
+        byte[] queries = mensaje.getBytes();
+        DataInputStream query = new DataInputStream(new ByteArrayInputStream(queries));
+
+        short Id = query.readShort(); // se usa short por lo que id es un espacio de 16 bits que se necesitan
+        short flags = query.readByte();
+
+        // PRIMARIOS EN EL ENCABEZADO
+        int Qr = (flags & 0x10000000) >>> 7;
+        int opCode = (flags & 0x01111000) >>> 3;
+        int aa = (flags & 0x00000100) >>> 2;
+        int tc = (flags & 0x00000010) >>> 1;
+        int rd = flags & 0x00000001;
+
+        // RESTANTES ENCABEZADO
+        int Ra = (flags & 0x10000000) >>> 7;
+        int z = (flags & 0x01110000) >>> 4;
+        int Rcode = flags & 0x00001111;
+
+        // RESTO DEL ENCABEZADO
+        short QDCount = query.readShort();
+        short ANCount = query.readShort();
+        short NSCount = query.readShort();
+        short ARCount = query.readShort();
+
+
+        Header queryRecibida = new Header(1, Id, z, Ra, rd, tc, aa, Qr,opCode,QDCount,ANCount,NSCount,ARCount); //LUEGO SE PUEDE PASAR ESTA CABECERA COMO PARAMETRO
+
+        String QName = "";
+        int longitud_nombre;
+        while ((longitud_nombre= query.readByte()) > 0) {
+            byte[] nombre = new byte[longitud_nombre];
+            for (int i = 0; i < longitud_nombre; i++) {
+                nombre[i] = query.readByte();
+            }
+            QName = new String(nombre, StandardCharsets.UTF_8); //se usa el standard char set
+        }
+        short QType = query.readShort();
+        short QClass = query.readShort();
+
+        String ipencontrada= GetIP(QName);
+        if(ipencontrada==null){
+            System.out.println("malditos inutiles hdpta");
+        }else{
+            System.out.println("se encontro la direccion buscada :))))");
+        }
 
     }
 
-    public String GetIP(String nombre){
+    public String GetIP(String nombre) {
         Iterator iter = Names.iterator();
-        int i=0;
-        boolean encontrada=false;
-        while (iter.hasNext()){
-            if(nombre.equals(iter)){
-                return Addresses.get(i); //esta parte falta comprobarla
+        int i = 0;
+        boolean encontrada = false;
+        while (iter.hasNext()) {
+            if (nombre.equals(iter)) {
+                return Addresses.get(i); // esta parte falta comprobarla
             }
             i++;
         }
-        if(!encontrada){
+        if (!encontrada) {
             return not_found(nombre);
         }
         return null;
     }
 
-    private String not_found(String nom_no_encontrado){
+    private String not_found(String nom_no_encontrado) {
 
-        //se supone que esta función entra cuando no se encuentre dentro del archivo maestro
-        DatagramPacket Mensaje= new DatagramPacket(nom_no_encontrado.getBytes(),nom_no_encontrado.length(),IPJAVERIANADNS, 53); //en teoria el puerto desde el rfc es el puerto 53
+        // se supone que esta función entra cuando no se encuentre dentro del archivo
+        // maestro
+        DatagramPacket Mensaje = new DatagramPacket(nom_no_encontrado.getBytes(), nom_no_encontrado.length(),
+                IPJAVERIANADNS, 53); // en teoria el puerto desde el rfc es el puerto 53
         try {
             this.SocketUDP.send(Mensaje);
-            String encontrada=null;
-            //this.socketUDP.receive();
+            String encontrada = null;
+            // this.socketUDP.receive();
             /*
-            RECEPCION DE MENSAJE:
-                tokenizacion....
-                separación de mensaje de recepción por componentes
-                addresses.add();
-                names.add();
+             * RECEPCION DE MENSAJE:
+             * tokenizacion....
+             * separación de mensaje de recepción por componentes
+             * addresses.add();
+             * names.add();
              */
             return encontrada;
         } catch (IOException e) {
--- src/main/java/org/example/Header.java
+++ src/main/java/org/example/Header.java
@@ -9,6 +9,12 @@ public class Header {
     private int TC=0;
     private int AA=0;
     private int QR=0;
+    private int RA = 0;
+    private short qdcount= 0;
+    private short ancount= 0;
+    private short nscount= 0;
+    private short arcount= 0;
+
     public Header(int tipo) { //este constructor solo se usa cuando se quiere un header de tipo 1 (respuesta)
         int mask=0;
         this.tipo=tipo | mask; //devuelve 1 si es tipo 1 (response) y 0 si es tipo 0 (query), manejandolo en forma de bits
@@ -21,15 +27,20 @@ public class Header {
         this.z= 000 | mask; // en teoria, si no hay errores, esto deberia ser un arreglo de 3 bits en 0
     }
 
-    public Header(int tipo, int id, int z, int RA, int RD, int TC, int AA, int Qr) {
+    public Header(int tipo, int id1, int z1, int RA1, int RD1, int TC1, int AA1, int Qr1, int opCode1, short qdcount1 ,short ancount1 , short nscount1, short arcount1) {
         this.tipo = tipo;
-        this.id = id;
-        this.z = z;
-        this.opCode = RA;
-        this.RD = RD;
-        this.TC = TC;
-        this.AA = AA;
-        this.QR =Qr;
+        this.id = id1;
+        this.z = z1;
+        this.opCode =opCode1;
+        this.RD = RD1;
+        this.TC = TC1;
+        this.AA = AA1;
+        this.QR =Qr1;
+        this.RA= RA1;
+        this.qdcount=qdcount1;
+        this.ancount= ancount1;
+        this.nscount= nscount1;
+        this.arcount= arcount1;
     }
 
 
